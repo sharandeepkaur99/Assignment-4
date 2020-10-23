025cdf7ad62d2aebefadf76415ccdcb942e9ac4d
sharandeepkaur99 <70535275+sharandeepkaur99@users.noreply.github.com>
2020-10-23 01:32:04 -0400

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded',() => {
+  document.querySelector("#quiz1).onsubmit = () => {
+    let quiz1_url = 'https://my-json-server.typicode.com/sharandeepkaur99/Assignment-4'
+    let question = document.querySelector("#question").value
